faf5272f06f262cfef24d2d70ba5d923908752d1
Fabio Oliveira <fabio.oliveira@lps.ufrj.br>
2024-10-30 13:40:25 +0000

    Tornando aplicacao do doppler em funcao

--- example/doppler_test.py
+++ example/doppler_test.py
@@ -1,54 +1,50 @@
+import typing
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as scipy
 
-# Constantes
+
+def apply_doppler(input_data: np.array, speeds: np.array, sound_speed: float):
+
+    num_blocks = len(speeds)
+    samples_per_block = len(input_data)//num_blocks
+
+    output = np.array([])
+    for i_block in range(num_blocks):
+        doppler_factor = (sound_speed + speeds[i_block]) / sound_speed
+
+        scaled_data = scipy.resample(
+                input_data[i_block * samples_per_block:(i_block+1) * samples_per_block - 1],
+                int(samples_per_block//doppler_factor))
+
+        output = np.concatenate((output, scaled_data))
+
+    return output
+
+
+
 sound_speed = 1500  # velocidade do som na água (m/s)
 sampling_rate = 16000  # taxa de amostragem do hidrofone (Hz)
-block_duration = 0.05  # duração de cada bloco de simulação (segundos)
-num_blocks = 100  # número de blocos
-num_samples_per_block = int(block_duration * sampling_rate)
+duration = 5
+n_samples = duration * sampling_rate
 
-# Parâmetros dos navios (faixas de velocidade e frequências)
-ship1_velocity_range = np.linspace(-10, 10, num_blocks)  # velocidades do navio 1
-ship2_velocity_range = np.linspace(-50, 50, num_blocks)  # velocidades do navio 2
+ship1_velocity_range = np.linspace(-10, 10, 100)  # velocidades do navio 1
+ship2_velocity_range = np.linspace(-50, 50, 100)  # velocidades do navio 2
 freq_ship1 = 6000  # frequência senoidal do navio 1 (Hz)
 freq_ship2 = 2000  # frequência senoidal do navio 2 (Hz)
 
-# Vetores para armazenar os sinais concatenados dos navios
-signal_ship1_concat = np.array([])
-signal_ship2_concat = np.array([])
+time = np.linspace(0, duration, n_samples, endpoint=False)
 
-# Loop para gerar cada bloco de sinal
-for i in range(num_blocks):
-    # Velocidades dos navios no bloco atual
-    ship1_velocity = ship1_velocity_range[i]
-    ship2_velocity = ship2_velocity_range[i]
 
-    # Fator Doppler para cada navio
-    doppler_factor_ship1 = (sound_speed + ship1_velocity) / sound_speed
-    doppler_factor_ship2 = (sound_speed + ship2_velocity) / sound_speed
+noise_ship1 = np.random.normal(0, 0.5, n_samples) + 2 * np.sin(2 * np.pi * freq_ship1 * time)
+noise_ship2 = np.random.normal(0, 0.5, n_samples) + 2 * np.sin(2 * np.pi * freq_ship2 * time)
 
-    # Gera o tempo comprimido/expandido para cada navio devido ao efeito Doppler
-    num_samples_doppler_ship1 = int(block_duration * sampling_rate * doppler_factor_ship1)
-    num_samples_doppler_ship2 = int(block_duration * sampling_rate * doppler_factor_ship2)
-    time_doppler_ship1 = np.linspace(0, block_duration * doppler_factor_ship1, num_samples_doppler_ship1, endpoint=False)
-    time_doppler_ship2 = np.linspace(0, block_duration * doppler_factor_ship2, num_samples_doppler_ship2, endpoint=False)
 
-    # Gera o sinal de ruído com componente senoidal para cada navio no domínio Doppler
-    noise_ship1 = np.random.normal(0, 0.5, num_samples_doppler_ship1) + 2 * np.sin(2 * np.pi * freq_ship1 * time_doppler_ship1)
-    noise_ship2 = np.random.normal(0, 0.5, num_samples_doppler_ship2) + 2 * np.sin(2 * np.pi * freq_ship2 * time_doppler_ship2)
+signal_ship1 = apply_doppler(noise_ship1, ship1_velocity_range, sound_speed)
+signal_ship2 = apply_doppler(noise_ship2, ship2_velocity_range, sound_speed)
 
-    # Re-amostra os sinais para a taxa de 16 kHz, para alinhar ao tempo do hidrofone
-    signal_ship1 = scipy.resample(noise_ship1, num_samples_per_block)
-    signal_ship2 = scipy.resample(noise_ship2, num_samples_per_block)
-
-    # Concatena os blocos no vetor final
-    signal_ship1_concat = np.concatenate((signal_ship1_concat, signal_ship1))
-    signal_ship2_concat = np.concatenate((signal_ship2_concat, signal_ship2))
-
-# Sinal combinado no hidrofone
-hydrophone_signal = signal_ship1_concat + signal_ship2_concat
+final_n_samples = np.min((len(signal_ship1), len(signal_ship2)))
+hydrophone_signal = signal_ship1[:final_n_samples] + signal_ship2[:final_n_samples]
 
 # Espectrograma do sinal combinado
 frequencies, times, Sxx = scipy.spectrogram(hydrophone_signal, fs=sampling_rate, nperseg=1024)
